f2a804bc785e10c2137865f9a18dae6113243494
DL <leinadmahpel.github@gmail.com>
2021-08-08 15:21:30 -0500

    tick marks added for x-axis complete

--- src/components/D3BarChart/D3BarChartWithAxes.tsx
+++ src/components/D3BarChart/D3BarChartWithAxes.tsx
@@ -86,13 +86,29 @@ function D3BarChartWithAxes(): JSX.Element {
                                     // to see props we can pass into SVGLineElement: https://developer.mozilla.org/en-US/docs/Web/SVG/Element/line
                                     // x1 is where the line starts in the x direction
                                     // since tickValue is a value from the scale's domain, we can use our xScale to map that tickValue from the domain to the range
-                                    <line 
-                                          x1={xScale(tickValue)} // top of line starts at
-                                          y1={0} // top height of line 
-                                          x2={xScale(tickValue)} // bottom of line ends at
-                                          y2={innerHeight} // bottom height of line
-                                          stroke="black" 
-                                    />
+                                    <g transform={`translate(${xScale(tickValue)}, ${0})`}>
+                                          <line 
+                                                // x1, y1, x2 are set to 0 by default so we can omit them if we want to
+                                                // x1={0} // top of line starts at
+                                                // y1={0} // top height of line 
+                                                // x2={0} // bottom of line ends at
+                                                y2={innerHeight} // bottom height of line
+                                                stroke="white" 
+                                          />
+                                          {/* 
+                                                // This will add a label for the given tick by using <text> element (could also add images here instead) 
+                                                // to set the text to be at the bottom, just set the y coordinate of the text to be innerHeight
+                                          */}
+                                          <text 
+                                                dy=".71em"
+                                                y={innerHeight + 3}
+                                                style={{
+                                                      textAnchor: 'middle'
+                                                }}
+                                          >
+                                                {tickValue}
+                                          </text>
+                                    </g>
                               ))}
                               {data.map((d: any) => (
                                     <rect 
